89bad9f0152eb9d10e42844d1feb77ec175f6c60
gnehs <jayuiop@gmail.com>
2022-10-23 13:47:05 +0800

    added PWA, PWA icons

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import './style.sass'
 import App from './App.vue'
-
+import { useRegisterSW } from 'virtual:pwa-register/vue';
+useRegisterSW({ immediate: true });
 createApp(App).mount('#app')
